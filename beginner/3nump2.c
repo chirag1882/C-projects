1ea3ce41914246dd659b514ee5b15b896541b6b7
chirag1882 <chiragyadavm17@gmail.com>
2025-02-12 15:14:50 +0530

    Add files via upload
    A program to find the largest of three nunbers!

---
+++ beginner/3nump2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, num3;
+
+    // Input three numbers
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    // Check if all numbers are equal
+    if (num1 == num2 && num2 == num3) {
+        printf("All numbers are equal: %d\n", num1);
+    } 
+    // Check which number is largest
+    else if (num1 >= num2 && num1 >= num3) {
+        printf("The largest number is: %d\n", num1);
+    } else if (num2 >= num1 && num2 >= num3) {
+        printf("The largest number is: %d\n", num2);
+    } else {
+        printf("The largest number is: %d\n", num3);
+    }
+
+    return 0;
+}
